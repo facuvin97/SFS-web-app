6e4fad8a9fe3bb18c1194325a08b9146dab4b1cb
Facu V <facundovinales@hotmail.com>
2024-08-11 18:35:09 -0300

    success MP

---
+++ src/components/SuccessAsociationMP.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { CircularProgress, Container, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../contexts/UserLogContext';
+
+const SuccessAsociationMP = () => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { userLog, setUserLog } = useUser();
+
+  const successAsociation = async (code) => {
+    try {
+      const response = await fetch('aca iria la url de modificar walker', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: userLog.id,
+          mercadopago: code,
+        }),
+      });
+
+      if (response.ok) {
+        // Actualiza campo mercadopago en userLog
+        setUserLog((prevUserLog) => ({
+          ...prevUserLog,
+          mercadopago: code,
+        }));
+
+        setLoading(false);
+        navigate('/success'); // Redirige a una página de éxito
+      } else {
+        console.error('Error al actualizar mercadopago');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+
+    }
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    if (code === null) {
+      navigate('/'); // No hay parámetro `code`, redirige al inicio
+    } else {
+      successAsociation(code); // Hay parámetro `code`, realiza la asociación
+    }
+  }, []);
+
+  return (
+    <Container
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      {loading ? (
+        <>
+          <CircularProgress />
+          <Typography variant="h6" style={{ marginTop: '16px' }}>
+            Cargando...
+          </Typography>
+        </>
+      ) : (
+        <Typography variant="h6">¡Carga completa! Lo estamos redireccionando...</Typography>
+      )}
+    </Container>
+  );
+};
+
+export default SuccessAsociationMP;
