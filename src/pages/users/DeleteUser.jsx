f77a5019fb5842e628a825126e2f589e3656a0ce
sergiobueno1983 <sergiobueno1983@gmail.com>
2024-12-12 09:49:26 -0300

    delet user useefect

--- src/pages/users/DeleteUser.jsx
+++ src/pages/users/DeleteUser.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../contexts/UserLogContext';
 
